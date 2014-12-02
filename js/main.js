1053c8c3b2668d9adb04c11e3b2ad2b63a5e42d7
Jacob Messina <jmessina@albany.edu>
2014-12-02 16:43:24 -0500

    something else

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('#btn-menu').click(function(){
+	$('.nav-main').slideToggle();
+});
+$("#panel1").click(function(){
+  $("#panelC1").toggle(1000);
+});
+//panel2
+$("#panel2").click(function(){
+  $("#panelC2").toggle(1000);
+});
+//panel3
+$("#panel3").click(function(){
+  $("#panelC3").toggle(1000);
+});
+
+$(document).ready(function(){
+    $(".nav-internal").sticky({topSpacing:5});
+  });
